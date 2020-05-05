552463e3a19a9d2ad6ea5f4a864bc65c09bb6ef9
fung <skytrc@163.com>
2020-05-05 12:57:44 +0800

    feat(map): 增加地图内容
    BaseMap：
    	-增添地图name
    	-增添坐标与元素绑定关系
    	-增添展示地图元素方法
    	-增添展示某一个元素方法
    	-增添判断坐标是否含有某个元素方法
    	-增添地图名映射地图类方法
    	-增添初始化方法

--- src/main/java/com/fung/server/content/entity/map/BaseMap.java
+++ src/main/java/com/fung/server/content/entity/map/BaseMap.java
@@ -16,29 +16,24 @@ public abstract class BaseMap {
     private int id;
 
     /**
-     * 地图网格
+     * 地图名称
      */
-    private int[][] grid = new int[3][3];
+    private String name;
 
     /**
-     * 地图中在线上的玩家
+     * 地图网格
+     */
+    private String[][] grid = new String[5][5];
+
+    /**
+     * 地图中在线上的玩家,以玩家id作为key，value为玩家
      */
     private Map<Integer, Player> mapPlayers;
 
     /**
-     * 设置传送门
+     * 传送元素对应的地图
      */
-    private Map<int[][], BaseMap> gate;
-
-    /**
-     * 设置地图连接门
-     *
-     * @param coordinate 需要连接的坐标
-     * @param map        连接下一个地图
-     */
-    public void setGate(int[][] coordinate, BaseMap map) {
-
-    }
+    private Map<String, BaseMap> gates;
 
     public void addPlayer(Player player) {
         mapPlayers.put(player.getId(), player);
@@ -50,6 +45,7 @@ public abstract class BaseMap {
 
     /**
      * 角色地图移动
+     *
      * @param oldMap 移动前的地图
      * @param newMap 移动后的地图
      * @param player 需要移动的角色
@@ -57,6 +53,75 @@ public abstract class BaseMap {
     public void playerMove(BaseMap oldMap, BaseMap newMap, Player player) {
         oldMap.removePlayer(player.getId());
         newMap.addPlayer(player);
+    }
+
+    /**
+     * 设置地图上的元素
+     *
+     * @param x       纵坐标
+     * @param y       横坐标
+     * @param element 元素，比如NPC，传送门等等
+     */
+    public void addElement(int x, int y, String element) {
+        grid[x][y] = element;
+    }
+
+    /**
+     * 展示地图中的元素
+     *
+     * @return 所有方格中的元素
+     */
+    public String showMapInfo() {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] != null) {
+                    res.append("位置 [").append(i).append(",").append(j).append("]")
+                            .append(" 拥有 ").append(grid[i][j]).append("\n");
+                }
+            }
+        }
+        return res.toString();
+    }
+
+    /**
+     * 展示某一格的元素
+     *
+     * @param x 纵坐标
+     * @param y 横坐标
+     * @return 指定格数的元素
+     */
+    public String showThisGridInfo(int x, int y) {
+        return "[" + x + "," + y + "]" + " 拥有 " + grid[x][y];
+    }
+
+    /**
+     * 判断某个坐标中是否含有某个元素
+     *
+     * @param x       纵坐标
+     * @param y       横坐标
+     * @param element 元素
+     */
+    public boolean hasElement(int x, int y, String element) {
+        return grid[x][y].equals(element);
+    }
+
+    /**
+     * 添加传送门，用map存储，名字对应地图
+     *
+     * @param name 地图对应的中文名
+     * @param map  地图类
+     */
+    public void addGate(String name, BaseMap map) {
+        gates.put(name, map);
+    }
+
+
+    /**
+     * 地图某些元素初始化
+     */
+    public void init() {
+
     }
 
     public int getId() {
@@ -67,11 +132,11 @@ public abstract class BaseMap {
         this.id = id;
     }
 
-    public int[][] getGrid() {
+    public String[][] getGrid() {
         return grid;
     }
 
-    public void setGrid(int[][] grid) {
+    public void setGrid(String[][] grid) {
         this.grid = grid;
     }
 
@@ -83,11 +148,19 @@ public abstract class BaseMap {
         this.mapPlayers = mapPlayers;
     }
 
-    public Map<int[][], BaseMap> getGate() {
-        return gate;
+    public Map<String, BaseMap> getGates() {
+        return gates;
     }
 
-    public void setGate(Map<int[][], BaseMap> gate) {
-        this.gate = gate;
+    public void setGates(Map<String, BaseMap> gates) {
+        this.gates = gates;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
