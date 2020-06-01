795d2c759f98846649b31fc196553991b8d63022
Skytrc <skytrc@163.com>
2020-06-01 23:51:26 +0800

    refactor:修改equipment初始化的位置
    新增
        -修改equipmentService，把GoodUtil的内容搬到Service中
        -增加equipment.xlsx中最大堆叠数

--- src/main/java/com/fung/server/content/service/impl/EquipmentServiceImpl.java
+++ src/main/java/com/fung/server/content/service/impl/EquipmentServiceImpl.java
@@ -1,12 +1,15 @@
 package com.fung.server.content.service.impl;
 
 import com.fung.server.content.dao.EquipmentDao;
+import com.fung.server.content.domain.equipment.EquipmentCreated;
 import com.fung.server.content.entity.Equipment;
+import com.fung.server.content.manager.EquipmentCreatedManager;
 import com.fung.server.content.service.EquipmentService;
-import com.fung.server.content.util.playerutil.GoodUtil;
 import com.fung.server.content.util.playerutil.PlayerUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author skytrc@163.com
@@ -16,7 +19,7 @@ import org.springframework.stereotype.Component;
 public class EquipmentServiceImpl implements EquipmentService {
 
     @Autowired
-    GoodUtil goodUtil;
+    EquipmentCreatedManager equipmentCreatedManager;
 
     @Autowired
     PlayerUtil playerUtil;
@@ -26,9 +29,39 @@ public class EquipmentServiceImpl implements EquipmentService {
 
     @Override
     public void createNewEquipment(String channelId, int equipmentId) {
-        Equipment equipment = goodUtil.getRandomValueEquipment(equipmentId);
+        EquipmentCreated created = equipmentCreatedManager.getEquipmentCreatedMap().get(equipmentId);
+        Equipment equipment = new Equipment();
+
         equipment.setPlayerId(playerUtil.getCurrentPlayer(channelId).getUuid());
+        equipment.setGoodId(equipmentId);
+        equipment.setGetTime(System.currentTimeMillis());
+        equipment.setQuantity(1);
+        equipment.setName(created.getName());
+        equipment.setRank(0);
+        equipment.setMaxDurable(created.getDurable());
+        equipment.setDurable(created.getDurable());
+        equipment.setMinLevel(created.getMinLevel());
+        equipment.setDescription(created.getDescription());
+        // 生成随机数值
+        equipment.setPlusHp(judgeMin(created.getMinHp(), created.getMaxHp()));
+        equipment.setPlusMp(judgeMin(created.getMinMp(), created.getMaxMp()));
+        equipment.setAttackPower(judgeMin(created.getMinPower(), created.getMaxPower()));
+        equipment.setMagicPower(judgeMin(created.getMinMagicPower(), created.getMaxMagicPower()));
+        equipment.setCriticalRate(judgeMin(created.getMinCriticalRate(), created.getMaxCriticalRate()));
+        equipment.setDefense(judgeMin(created.getMinDefense(), created.getMaxDefense()));
         equipmentDao.updateEquipment(equipment);
 
+    }
+
+    /**
+     * @param minValue 装备最低数值
+     * @param maxValue 装备最高数值
+     * @return 范围内随机值
+     */
+    public int judgeMin(int minValue, int maxValue) {
+        if (minValue == 0) {
+            return 0;
+        }
+        return ThreadLocalRandom.current().nextInt(minValue, maxValue);
     }
 }
--- src/main/java/com/fung/server/content/util/playerutil/GoodUtil.java
+++ src/main/java/com/fung/server/content/util/playerutil/GoodUtil.java
@@ -1,12 +1,6 @@
 package com.fung.server.content.util.playerutil;
 
-import com.fung.server.content.entity.Equipment;
-import com.fung.server.content.domain.equipment.EquipmentCreated;
-import com.fung.server.content.manager.EquipmentCreatedManager;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author skytrc@163.com
@@ -14,49 +8,4 @@ import java.util.concurrent.ThreadLocalRandom;
  */
 @Component
 public class GoodUtil {
-
-    @Autowired
-    EquipmentCreatedManager equipmentCreatedManager;
-
-    /**
-     * 新建装备实体随机赋值
-     * @param equipmentId 装备Id
-     * @return 装备
-     */
-    public Equipment getRandomValueEquipment(int equipmentId) {
-        EquipmentCreated created = equipmentCreatedManager.getEquipmentCreatedMap().get(equipmentId);
-        Equipment equipment = new Equipment();
-
-        equipment.setGoodId(equipmentId);
-        equipment.setGetTime(System.currentTimeMillis());
-        equipment.setQuantity(1);
-        equipment.setName(created.getName());
-        equipment.setRank(0);
-        equipment.setMaxDurable(created.getDurable());
-        equipment.setDurable(created.getDurable());
-        equipment.setMinLevel(created.getMinLevel());
-        equipment.setDescription(created.getDescription());
-        // 生成随机数值
-        equipment.setPlusHp(judgeMin(created.getMinHp(), created.getMaxHp()));
-        equipment.setPlusMp(judgeMin(created.getMinMp(), created.getMaxMp()));
-        equipment.setAttackPower(judgeMin(created.getMinPower(), created.getMaxPower()));
-        equipment.setMagicPower(judgeMin(created.getMinMagicPower(), created.getMaxMagicPower()));
-        equipment.setCriticalRate(judgeMin(created.getMinCriticalRate(), created.getMaxCriticalRate()));
-        equipment.setDefense(judgeMin(created.getMinDefense(), created.getMaxDefense()));
-
-        return equipment;
-    }
-
-    /**
-     * @param minValue 装备最低数值
-     * @param maxValue 装备最高数值
-     * @return 范围内随机值
-     */
-    public int judgeMin(int minValue, int maxValue) {
-        if (minValue == 0) {
-            return 0;
-        }
-        return ThreadLocalRandom.current().nextInt(minValue, maxValue);
-    }
-
 }
