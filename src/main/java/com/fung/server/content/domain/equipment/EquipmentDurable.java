fe40c4de4391c7a3cadc2d95b01471b01165802c
fung <skytrc@163.com>
2020-06-10 19:57:15 +0800

    feat: 新增装备创建工厂、装备耐久计算
    新增
    	-装备创建工厂：用于创建装备，数值随机。
    	-装备耐久计算，用于攻击或者被攻击时使用，计算装备耐久

---
+++ src/main/java/com/fung/server/content/domain/equipment/EquipmentDurable.java
@@ -0,0 +1,52 @@
+package com.fung.server.content.domain.equipment;
+
+import com.fung.server.content.config.good.equipment.EquipmentType;
+import com.fung.server.content.domain.calculate.PlayerValueCalculate;
+import com.fung.server.content.entity.Equipment;
+import com.fung.server.content.entity.Player;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * 用于计算用户装备耐久度，当耐久度为零时，装备加成效果为0
+ * @author skytrc@163.com
+ * @date 2020/6/9 17:39
+ */
+@Component
+public class EquipmentDurable {
+
+    @Autowired
+    private PlayerValueCalculate playerValueCalculate;
+
+    public void equipmentDurableMinus(Player player, boolean isDefense) {
+        boolean isZero = false;
+        List<Equipment> equipments = player.getEquipments();
+        for (Equipment equipment : equipments) {
+            // 判断是否为防御操作
+            if (isDefense) {
+                if (!EquipmentType.isDefenseType(equipment)) {
+                    continue;
+                }
+            } else {
+                if (EquipmentType.isDefenseType(equipment)) {
+                    continue;
+                }
+            }
+            // 判断原来装备的耐久是否为0
+            if (equipment.getDurable() == 0) {
+                continue;
+            }
+            int durable = equipment.getDurable() - 1;
+            // 如果装备耐久掉到0，重新计算玩家数值
+            if (durable == 0) {
+                isZero = true;
+            }
+            equipment.setDurable(durable);
+        }
+        if (isZero) {
+            playerValueCalculate.calculatePlayerBaseValue(player);
+        }
+    }
+}
